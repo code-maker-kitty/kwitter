b15a518f34a10d1b9347c6f57e2157c78ba7e57c
code-maker-kitty <77761014+code-maker-kitty@users.noreply.github.com>
2021-05-07 14:28:29 -0700

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -12,6 +12,7 @@ var firebaseConfig = {
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     user_name=localStorage.getItem("user_name");
+    room_name=localStorage.getItem("room_name");
     document.getElementById("user_name").innerHTML="Welcome"+user_name+"!";
     function addRoom(){
           room_name=document.getElementById("room_name").value;
@@ -35,4 +36,9 @@ function redirectToRoomName(name){
       console.log(name);
       localStorage.setItem("room_name",name);
       window.location="kwitter_page.html";
+}
+function logout(){
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location="kwitter.html";
 }
